f56876b03129585353d39aad55244d88c09a44f4
Chris Burton <csburton@gmail.com>
2025-12-30 13:32:53 +0000

    ADding casted value helper

--- src/Model/CategoryField.php
+++ src/Model/CategoryField.php
@@ -5,6 +5,7 @@ namespace Pantono\Products\Model;
 use Pantono\Contracts\Attributes\Locator;
 use Pantono\Products\Categories;
 use Pantono\Contracts\Attributes\FieldName;
+use Pantono\Utilities\DateTimeParser;
 
 class CategoryField
 {
@@ -41,5 +42,30 @@ class CategoryField
     public function setValue(string $value): void
     {
         $this->value = $value;
+    }
+
+    public function getCastedValue(): mixed
+    {
+        $type = $this->getType()?->getType();
+        $value = $this->getValue();
+        if ($type === 'boolean') {
+            return (bool)$value;
+        }
+        if ($type === 'integer') {
+            return (int)$value;
+        }
+        if ($type === 'float') {
+            return (float)$value;
+        }
+        if ($type === 'date' || $type === 'datetime') {
+            return DateTimeParser::parseDateImmutable($value);
+        }
+        if ($type === 'json') {
+            return json_decode($value, true, 512, JSON_THROW_ON_ERROR);
+        }
+        if ($type === 'array') {
+            return explode(',', $value);
+        }
+        return $value;
     }
 }
