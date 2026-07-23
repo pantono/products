a53e7729759ba71c8dcbd62d262dc7cae2e03d68
Chris Burton <csburton@gmail.com>
2026-07-23 21:07:20 +0100

    Typoe

--- migrations/20090100100007_product_history_migration.php
+++ migrations/20090100100007_product_history_migration.php
@@ -6,7 +6,7 @@ use Pantono\Database\Migration\Base\BasePantonoMigration;
 
 final class ProductHistoryMigration extends BasePantonoMigration
 {
-    public function up(): void
+    public function change(): void
     {
         $this->table($this->addTablePrefix('product_history'))
             ->addLinkedColumn('product_id', $this->addTablePrefix('product'), 'id')
