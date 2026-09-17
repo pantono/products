a32817f1501c1e435503eed7a8be6c23cc521a32
Chris Burton <csburton@gmail.com>
2026-09-17 20:14:11 +0100

    ADding date time type

--- src/Model/ProductField.php
+++ src/Model/ProductField.php
@@ -69,6 +69,9 @@ class ProductField
         if ($type === 'date') {
             return DateTimeParser::parseDate($this->getValue())?->format('Y-m-d');
         }
+        if ($type === 'datetime') {
+            return DateTimeParser::parseDate($this->getValue())?->format('Y-m-d H:i:s');
+        }
         return $this->getValue();
     }
 }
